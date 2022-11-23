b7cec39751f53f5dbd49a8dcae74ac26df3bf06d
Alex Ford <alex.ford@nationalarchives.gov.uk>
2022-11-23 08:37:05 +0000

    Update Secrets

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-import os
 import setuptools
 
-tre_lib_version = os.environ['TRE_LIB_VERSION']
-
 setuptools.setup(
-    name='tre_lib',
-    version=tre_lib_version,
-    description='TRE Python code library',
-    packages={'tre_lib'},
-    package_dir={'tre_lib': 'tre_lib'},
+    name='trelib',
+    version="0.0.3",
+    description='TRE Events Library',
+    packages=['tre_lib'],
     package_data={'tre_lib': ['schema*.json']},
     python_requires='>=3.8'
 )
--- version.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-export TRE_LIB_VERSION=0.0.2
