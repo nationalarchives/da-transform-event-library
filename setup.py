e068df36599d9a0c5e8b9892b32effa97e74f9a2
Alex Ford <alex.ford@nationalarchives.gov.uk>
2022-11-29 09:55:45 +0000

    tre_lib_event in setup.py changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='trelib',
     version="0.0.4",
     description='TRE Events Library',
-    packages=['tre_event_lib.tre_schemas', 'tre_lib'],
+    packages=['tre_event_lib.tre_schemas', 'tre_event_lib'],
     package_data={'tre_event_lib.tre_schemas': ['*.json']},
     python_requires='>=3.8'
 )
